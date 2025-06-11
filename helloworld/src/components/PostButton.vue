39e99c4a730871d0befb7edd614f0d5f97e66e72
home office <cristian.ruizdiaz.tec@gmail.com>
2025-06-10 21:18:25 -0300

    2.Composition

---
+++ helloworld/src/components/PostButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <button class="post-btn">
+        Acceder
+    </button>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.post-btn {
+    padding: 15px;  /* relleno */
+    background-color: blueviolet;    /* Color de fondo */
+    color: white;
+    border: 0;
+    outline: 0;
+}
+.post-btn:hover{
+    background-color: white;
+    color: blueviolet;
+}
+</style>
+
---
+++ helloworld/src/components/PostCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="post-card">
+        <h1>Titulo</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea modi eaque explicabo rerum eligendi, fuga cumque unde tenetur eos quis atque aut iusto quasi minima perferendis voluptas, veniam, est corporis?</p>    
+        <postButton/>
+    </div>
+</template>
+
+<script setup>
+import postButton from './PostButton.vue';
+</script>
+
+<style scoped>
+.post-card{
+    border-radius: 15px;
+    padding: 10px;
+    margin: 5px;
+    border: 1px solid #ccc;
+}
+
+</style>
